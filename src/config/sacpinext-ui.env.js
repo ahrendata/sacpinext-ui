451afff88156a95c541a94053955fed0a0d9b598
ChanoHL <shanohl.sist@gmail.com>
2019-10-14 16:00:59 -0500

    add buidl.sh

--- src/config/sacpinext-ui.env.js
+++ src/config/sacpinext-ui.env.js
@@ -1,4 +1,4 @@
 window.SacpiNextUIEnv = {
-    "apiUrl": "http://localhost:62381/api"//"{{ .Env.SACPI_API_URL }}"
+    //"apiUrl": "http://localhost:62381/api"//"{{ .Env.SACPI_API_URL }}"
     //"apiUrl": "http://apinext.couchpotatofries.org:8788/api"
 };
